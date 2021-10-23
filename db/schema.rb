096ce26d1c77235694cb914890a8756ecaf89eee
Hugues Bernet-Rollande <hugues@xdev.fr>
2021-10-23 11:44:58 +0200

    bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_094349) do
+ActiveRecord::Schema.define(version: 2021_10_23_094432) do
+
+  create_table "chats", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "text"
