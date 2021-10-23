15acb8102559a45e536fcc21eee8dc15ab2faff0
Hugues Bernet-Rollande <hugues@xdev.fr>
2021-10-23 11:43:32 +0200

    bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_124329) do
+ActiveRecord::Schema.define(version: 2021_10_23_094302) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
