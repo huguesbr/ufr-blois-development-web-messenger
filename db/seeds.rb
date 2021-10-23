5b742a935009494110245afe0483f55b8c7fed6f
Hugues Bernet-Rollande <hugues@xdev.fr>
2021-10-23 12:09:43 +0200

    adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# creating some users
+%w(Hugues Talia Naelie Kian).each do |name|
+  User.create(name: name)
+end
+
+# creating some chats
+%w(Development Fun Random).each do |name|
+  Chat.create(name: name, user: User.all.sample)
+end
+
+# creating some messages
+['Hello, world', "What's up", "How are you?", "Coming tonight?", "No", "Yes"].each do |text|
+  Message.create(chat: Chat.all.sample, user: User.all.sample, text: text)
+end
