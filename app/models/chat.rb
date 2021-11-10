e1bf33584c888155a6afef05ee9cb47ba4b53258
Hugues Bernet-Rollande <hugues@xdev.fr>
2021-11-10 14:32:19 +0100

    create a membership for user when creating chat

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,10 @@
 class Chat < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
+
+  # ensure chat's creator have a membership
+  # https://apidock.com/rails/ActiveRecord/Callbacks/after_create
+  after_create do |chat|
+    Membership.create(user: chat.user, chat: chat)
+  end
 end
