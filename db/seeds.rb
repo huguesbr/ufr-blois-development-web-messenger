b4941a05ed3ac43cd2e74004c5a87be7402fdfde
Hugues Bernet-Rollande <hugues@xdev.fr>
2021-11-10 14:21:10 +0100

    update `seed.rb` to add membership for chat's owner / message's owner

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,15 @@ end
 ['Hello, world', "What's up", "How are you?", "Coming tonight?", "No", "Yes"].each do |text|
   Message.create(chat: Chat.all.sample, user: User.all.sample, text: text)
 end
+
+# ensure that each chat owner have a membership
+Chat.all.each do |chat|
+  Membership.create(chat_id: chat.id, user_id: chat.user_id)
+end
+
+# ensure that each chat's message owner have a membership
+Message.all.each do |message|
+  # https://apidock.com/rails/v4.0.2/ActiveRecord/Relation/find_or_create_by
+  # Finds the first record with the given attributes, or creates a record with the attributes if one is not found
+  Membership.find_or_create_by(chat_id: message.chat_id, user_id: message.user_id)
+end
