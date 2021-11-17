037af71b35ffea0f188b015a899ee1bdaf0e758c
Hugues Bernet-Rollande <hugues@xdev.fr>
2021-11-17 17:08:44 +0100

    updating application controller to lookup current user by `session_token` vs `current_user_id`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,25 @@ class ApplicationController < ActionController::API
     # https://apidock.com/rails/v4.0.2/ActiveRecord/FinderMethods/find_by
     # find_by(id: ) will return nil if user not found
     # find(id) will raise an error if user not found
-    @current_user ||= User.find_by(id: current_user_id)
+    # find user associated with *eventual* session (`&.`)
+    @current_user ||= current_session&.user
+    raise UnauthorizedError unless @current_user
+
+    @current_user
+  end
+
+  def current_session_token
+    # grab session token from params
+    @current_session_token ||= params[:session_token]
+  end
+
+  def current_session
+    # find session for session token
+    @current_session ||= Session.find_by(token: current_session_token)
   end
 
   def current_user_id
-    @current_user_id ||= params[:current_user_id]&.to_i
+    @current_user_id ||= current_user.id
   end
 
   def verify_user_presence
