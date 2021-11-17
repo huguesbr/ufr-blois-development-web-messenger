06f168dd87bdfcaf2961b2838f38ab0086c05b9c
Hugues Bernet-Rollande <hugues@xdev.fr>
2021-11-17 17:16:34 +0100

    `bin/rake db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_155621) do
+ActiveRecord::Schema.define(version: 2021_11_17_161540) do
 
   create_table "chats", force: :cascade do |t|
     t.string "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_11_17_155621) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
